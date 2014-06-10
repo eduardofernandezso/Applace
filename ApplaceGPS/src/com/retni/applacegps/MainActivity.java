b1a2acf72d97c7b19be9a2c802001d9175d0a22e
eduardo.fernandez <eduardo.fernandez@alumnos.inf.utfsm.cl>
2014-06-10 00:29:06 -0400

    se subieron los ultimos cambios de mapa, icono y mas

--- ApplaceGPS/src/com/retni/applacegps/MainActivity.java
+++ ApplaceGPS/src/com/retni/applacegps/MainActivity.java
@@ -3,11 +3,16 @@ package com.retni.applacegps;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+
+//import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.SimpleLocationOverlay;
 import org.osmdroid.api.IMapController;
@@ -17,35 +22,82 @@ import org.osmdroid.util.GeoPoint;
 public class MainActivity extends Activity {
 	
 	TextView ubicacion;
-
+	private MapView mapView;
+	private IMapController mapController;
+	double lat=0;
+	double lon=0;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		LocationManager milocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		LocationListener milocListener = new MiLocationListener();
+		milocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 1000*2, 10, milocListener);
 		
 		ubicacion = (TextView)findViewById(R.id.ubicacion);
 		
-		MapView mapView = (MapView) findViewById(R.id.mapView);
-		mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+		mapView = (MapView) findViewById(R.id.mapView);
+		mapView.setTileSource(TileSourceFactory.MAPNIK);
 		mapView.setMultiTouchControls(true);
-		IMapController mapController = mapView.getController();
+		mapView.setBuiltInZoomControls(true);
+		mapController = mapView.getController();	
 		mapController.setZoom(18);
-		
+			
 		Location valor = getMyLocation();
-		ubicacion.setText("Latitud: "+valor.getLatitude()+" y Longitud: "+valor.getLongitude());
+		if(valor==null){
+			Toast.makeText( getApplicationContext(),"No se puede acceder",Toast.LENGTH_SHORT ).show();
+		}
+		else{
+			milocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 1000*2, 10, milocListener);
+			ubicacion.setText("Latitud: "+valor.getLatitude()+" y Longitud: "+valor.getLongitude());
+			do{	
+				GeoPoint myLocation = new GeoPoint(getMyLocation());
+					
+				SimpleLocationOverlay myLocationOverlay = new SimpleLocationOverlay(this);
+				mapView.getOverlays().add(myLocationOverlay);
+		 
+				mapController.setCenter(myLocation);
+				myLocationOverlay.setLocation(myLocation);
+				lat=valor.getLatitude();
+				lon=valor.getLongitude();
 				
-		GeoPoint myLocation = new GeoPoint(getMyLocation());
-				
-		SimpleLocationOverlay myLocationOverlay = new SimpleLocationOverlay(this);
-		mapView.getOverlays().add(myLocationOverlay);
- 
-		mapController.setCenter(myLocation);
-		myLocationOverlay.setLocation(myLocation);
+			}
+			while(lat!=valor.getLatitude() || lon!=valor.getLongitude());
+		}		
 	}
+	
 	
 	Location getMyLocation(){
 		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 			return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 		}
+	
+	public class MiLocationListener implements LocationListener
+	{
+		@Override
+		public void onLocationChanged(Location loc){
+			lat = loc.getLatitude();
+			lon = loc.getLongitude();
+			String coordenadas = "Mis coordenadas son: " + "Latitud = " + lat + " Longitud = " + lon;
+			ubicacion.setText(coordenadas);
+			Toast.makeText( getApplicationContext(),"Cambio ubicacion",Toast.LENGTH_SHORT ).show();
+		}
+		
+		@Override
+		public void onProviderDisabled(String provider){
+			Toast.makeText( getApplicationContext(),"GpsApplace Desactivado",Toast.LENGTH_SHORT ).show();
+		}
+		
+		@Override
+		public void onProviderEnabled(String provider){
+			Toast.makeText( getApplicationContext(),"GpsApplace Activado",Toast.LENGTH_SHORT ).show();
+		}
+		
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras){
+			
+		}
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
