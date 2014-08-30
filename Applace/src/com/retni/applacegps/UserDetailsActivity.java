a590cc2c58b0a64d732d53ce7d6541d4d3f2e2b1
eduardofernandezso <eduardo.fernandezso@alumnos.usm.cl>
2014-08-30 16:28:49 -0400

    Revert "eliminando imagenes y archivos basura"
    This reverts commit 2c6f05a66b69509a30a8ba25a741dcedbdfde41b.

---
+++ Applace/src/com/retni/applacegps/UserDetailsActivity.java
@@ -0,0 +1,190 @@
+package com.retni.applacegps;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.ProfilePictureView;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+//import com.parse.logindeface.R;
+
+
+public class UserDetailsActivity extends Activity {
+
+	private ProfilePictureView userProfilePictureView;
+	private TextView userNameView;
+	private TextView userEmailView;
+
+	private Button logoutButton;
+	private Button bshare;
+
+	String Fnombre,Fmail;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.userdetails);
+
+		userProfilePictureView = (ProfilePictureView) findViewById(R.id.userProfilePicture);
+		userNameView = (TextView) findViewById(R.id.userName);
+		userEmailView = (TextView) findViewById(R.id.Email);
+
+		logoutButton = (Button) findViewById(R.id.logoutButton);
+		logoutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLogoutButtonClicked();
+			}
+		});
+		
+		
+		bshare = (Button) findViewById(R.id.bshare);
+		bshare.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onShare();
+			}
+		});
+
+
+		// Fetch Facebook user info if the session is active
+		Session session = ParseFacebookUtils.getSession();
+		if (session != null && session.isOpened()) {
+			makeMeRequest();
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			// Check if the user is currently logged
+			// and show any cached content
+			updateViewsWithProfileInfo();
+		} else {
+			// If the user is not logged in, go to the
+			// activity showing the login view.
+			startLoginActivity();
+		}
+	}
+
+	private void makeMeRequest() {
+		Request request = Request.newMeRequest(ParseFacebookUtils.getSession(),
+				new Request.GraphUserCallback() {
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						if (user != null) {
+							// Create a JSON object to hold the profile info
+							JSONObject userProfile = new JSONObject();
+							try {
+								// Populate the JSON object
+								userProfile.put("facebookId", user.getId());
+								userProfile.put("name", user.getName());
+								if (user.getProperty("email") != null) {
+									userProfile.put("email", (String) user
+											.getProperty("email"));
+								}
+								// Save the user profile info in a user property
+								ParseUser currentUser = ParseUser
+										.getCurrentUser();
+								currentUser.put("profile", userProfile);
+								currentUser.saveInBackground();
+
+								// Show the user info
+								updateViewsWithProfileInfo();
+							} catch (JSONException e) {
+								Log.d(logindeface.TAG,
+										"Error parsing returned user data.");
+							}
+
+						} else if (response.getError() != null) {
+							if ((response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_RETRY)
+									|| (response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_REOPEN_SESSION)) {
+								Log.d(logindeface.TAG,
+										"The facebook session was invalidated.");
+								onLogoutButtonClicked();
+							} else {
+								Log.d(logindeface.TAG,
+										"Some other error: "
+												+ response.getError()
+														.getErrorMessage());
+							}
+						}
+					}
+				});
+		request.executeAsync();
+
+	}
+
+	private void updateViewsWithProfileInfo() {
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser.get("profile") != null) {
+			JSONObject userProfile = currentUser.getJSONObject("profile");
+			try {
+				if (userProfile.getString("facebookId") != null) {
+					String facebookId = userProfile.get("facebookId")
+							.toString();
+					userProfilePictureView.setProfileId(facebookId);
+				} else {
+					// Show the default, blank user profile picture
+					userProfilePictureView.setProfileId(null);
+				}
+				if (userProfile.getString("name") != null) {
+					Fnombre = userProfile.getString("name");
+					userNameView.setText(userProfile.getString("name"));
+				} else {
+					userNameView.setText("");
+				}
+				if (userProfile.getString("email") != null) {
+					Fmail = userProfile.getString("email");
+					userEmailView.setText(userProfile.getString("email"));
+				} else {
+					userEmailView.setText("");
+				}
+			} catch (JSONException e) {
+				Log.d(logindeface.TAG,
+						"Error parsing saved user data.");
+			}
+
+		}
+	}
+
+	private void onLogoutButtonClicked() {
+		// Log the user out
+		ParseUser.logOut();
+
+		// Go to the login view
+		startLoginActivity();
+	}
+
+	private void startLoginActivity() {
+		Intent intent = new Intent(this, MainActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+	}
+	private void onShare() {
+		Intent intent = new Intent(UserDetailsActivity.this, Activity_iniciarSesion.class);
+		
+		
+		//intent.putExtra("n", Fnombre);
+		//intent.putExtra("m", Fmail);
+		
+		startActivity(intent);
+	}
+}
