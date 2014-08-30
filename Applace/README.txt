3c3f348fd6062bb37016c700e7546400f763a601
eduardofernandezso <eduardo.fernandezso@alumnos.usm.cl>
2014-08-30 16:23:16 -0400

    Revert "eliminando imagenes y archivos basura"
    This reverts commit 2c6f05a66b69509a30a8ba25a741dcedbdfde41b.

---
+++ Applace/README.txt
@@ -0,0 +1,30 @@
+================================================================================
+Consideraciones ================================================================
+================================================================================
+
+ATENCION:
+    Esta aplicación posee un Login mediante Facebook, pero para poder utilizar este se debe
+    poseer permisos especiales desde Facebook Developers, los cuales deben ser concedidos
+    por un administrador de la aplicacion en Facebook, mediante el registro de la Key Hashes
+    obtenida del computador del cual se esta ejecutando la aplicación.
+  
+WARNING:
+    android - Login failed invalid key error with Facebook SDK
+
+Usos permitidos:
+----------------
+
+	Para acceder a las funcionalidades del mapa se recomienda realizar el registro
+	mediante el correo y luego iniciar sesión con este.
+
+
+Otros:
+----------------------
+
+	Para mayor información de la aplicación en Facebook, Visitar:
+
+		- https://www.facebook.com/games/applaceog/?fbs=-1&preview=1&locale=es_LA
+
+================================================================================
+Versión 1.0 de Applace =========================================================
+================================================================================
---
+++ Applace/src/com/retni/applacegps/logindeface.java
@@ -0,0 +1,97 @@
+package com.retni.applacegps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+//import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+public class logindeface extends Activity {
+
+	static final String TAG = "MyApp";
+	private Button loginButton;
+	private Dialog progressDialog;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.face1);
+		Parse.initialize(this, "XyEh8xZwVO3Fq0hVXyalbQ0CF81zhcLqa0nOUDY3", "bK1hjOovj0GAmgIsH6DouyiWOHGzeVz9RxYc6vur");
+
+		// Set your Facebook App Id in strings.xml
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+		
+		loginButton = (Button) findViewById(R.id.loginButton);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLoginButtonClicked();
+			}
+		});
+		// Check if there is a currently logged in user
+		// and they are linked to a Facebook account.
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the user info activity
+			showUserDetailsActivity();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+	}
+
+	private void onLoginButtonClicked() {
+		logindeface.this.progressDialog = ProgressDialog.show(
+				logindeface.this, "", "Logging in...", true);
+		List<String> permissions = Arrays.asList("public_profile","email","user_likes");
+		ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, ParseException err) {
+				logindeface.this.progressDialog.dismiss();
+				if (user == null) {
+					Log.d(logindeface.TAG,
+							"Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					Log.d(logindeface.TAG,
+							"User signed up and logged in through Facebook!");
+					showUserDetailsActivity();
+				} else {
+					Log.d(logindeface.TAG,
+							"User logged in through Facebook!");
+					showUserDetailsActivity();
+				}
+			}
+		});
+	}
+
+	private void showUserDetailsActivity() {
+		//ParseUser user = ParseUser.getCurrentUser();
+		//user.setUsername();
+		Intent intent = new Intent(logindeface.this, Logueado.class);
+		startActivity(intent);
+	}
+}
